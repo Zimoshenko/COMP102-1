79223a06cc1278e5585b13693f18503afc33b617
UY114514 <UY114514@users.noreply.github.com>
2017-11-20 09:53:54 +0800

    core part of ImageRenderer

--- Assiginment4/ImageRenderer/ImageRenderer.java
+++ Assiginment4/ImageRenderer/ImageRenderer.java
@@ -1,3 +1,4 @@
+
 // This program is copyright VUW.
 // You are granted permission to use it to construct your answer to a ${course} assignment.
 // You may not distribute it in any other way without permission.
@@ -15,28 +16,36 @@ import java.awt.Color;
  *  ppm images are the simplest possible colour image format.
  */
 
-public class ImageRenderer{
-    public static final int TOP = 20;   // top edge of the image
-    public static final int LEFT = 20;  // left edge of the image
-    public static final int PIXEL_SIZE = 2;  
+public class ImageRenderer {
+    public static final int TOP = 20; // top edge of the image
+    public static final int LEFT = 20; // left edge of the image
+    public static final int PIXEL_SIZE = 2;
 
     // Constructor
     public ImageRenderer() {
-           UI.initialise();
-           UI.addButton("Clear", UI::clearGraphics );
-           UI.addButton("Render (core)", this::doRenderImageCore );
-           UI.addButton("Render (compl)", this::doRenderAnimatedImage );
-           UI.addButton("Quit", UI::quit );
-           UI.setWindowSize(850, 700);
-           UI.setDivider(0.0);
+        UI.initialise();
+        UI.addButton("Clear", UI::clearGraphics);
+        UI.addButton("Render (core)", this::doRenderImageCore);
+        UI.addButton("Render (compl)", this::doRenderAnimatedImage);
+        UI.addButton("Quit", UI::quit);
+        UI.setWindowSize(850, 700);
+        UI.setDivider(0.0);
     }
 
     /** Core:
         * Renders a ppm image file.
         * Asks for the name of the file, then calls renderImageHelper.
         */
-    public void doRenderImageCore(){
-           /*# YOUR CODE HERE */
+    public void doRenderImageCore() {
+        //String fileName = UI.askString("Input name of the file:");
+        try {
+            Scanner scan = new Scanner(new File("1-image-rose.ppm"));
+            this.renderImageHelper(scan);
+        } catch (Exception e) {
+            //TODO: handle exception
+        }
+
+        /*# YOUR CODE HERE */
 
     }
 
@@ -51,10 +60,51 @@ public class ImageRenderer{
         * The first four tokens are "P3", number of columns, number of rows, 255
         * The remaining tokens are the pixel values (red, green, blue for each pixel)
         */
-    public void renderImageHelper(Scanner sc){
-           /*# YOUR CODE HERE */
-
+    public Color colorHelper(int r,int g,int b) {
+        Color color = new Color(r, g, b);
+        return color;
+        
     }
+    
+    
+        public void renderImageHelper(Scanner sc) {
+        /*# YOUR CODE HERE */
+        //    if (!sc.next().equals("P3")) {
+        //    UI.println("File is not a ppm image");
+        //    }
+        //    else{
+        //    }
+        sc.next();
+        int cols = sc.nextInt();
+        int rows = sc.nextInt();
+        int r,g,b;
+        sc.nextInt();
+
+        int row = 0;
+        int col = 0;
+        int x = 0;
+        int y = 0;
+        int i =0;
+
+        while (row != rows) {
+            while (col != cols) {
+                    r = sc.nextInt();
+                    g = sc.nextInt();
+                    b = sc.nextInt();
+                    UI.setColor(colorHelper(r,g,b));
+                    UI.fillRect(x + col * PIXEL_SIZE, y + row * PIXEL_SIZE, PIXEL_SIZE, PIXEL_SIZE);
+
+                    col++;
+                }
+                col = 0;
+                row++;
+            }
+
+        }
+
+
+
+    
 
     /** Completion
         * Renders a ppm image file which may be animated (multiple images in the file)
@@ -63,11 +113,9 @@ public class ImageRenderer{
         * Renders each image in the file in turn with 200 mSec delay.
         * Repeats the sequence 3 times.
         */
-    public void doRenderAnimatedImage(){
-           /*# YOUR CODE HERE */
+    public void doRenderAnimatedImage() {
+        /*# YOUR CODE HERE */
 
     }
-
-
 
 }
